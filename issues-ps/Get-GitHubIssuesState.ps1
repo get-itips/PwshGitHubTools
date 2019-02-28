efabeb41adac45948f55080f0c797b9e4bbf75d2
get-itips <andresg@capazero.net>
2019-02-28 15:44:21 -0300

    Added OAuth Support

--- issues-ps/Get-GitHubIssuesState.ps1
+++ issues-ps/Get-GitHubIssuesState.ps1
@@ -11,7 +11,11 @@ param(
     [string]$Repo="",
 
     [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
-    [string[]]$Issues
+    [string[]]$Issues,
+
+    [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+    [string]$OAuthToken
+
 )
 #sample uri: "https://api.github.com/repos/MicrosoftDocs/office-docs-powershell/issues/1300"
 
@@ -19,12 +23,17 @@ $uri1stPart="https://api.github.com/repos/"
 
 $uri2ndPart = $uri1stPart + $Owner + "/" + $Repo + "/issues/" 
 
+$headers = @{
+    'token' = $OAuthToken
+}
+
+
 Write-Host "Starting report on requested issues"
 Write-Host "##################################"
 foreach($issue in $issues){
 
     $uri = $uri2ndPart + $issue
-    $result=Invoke-RestMethod -uri $uri
+    $result=Invoke-RestMethod -uri $uri -Headers $headers
     
     Write-Host $result.number -NoNewline
     Write-host " "-NoNewline
